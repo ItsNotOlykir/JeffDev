548f1b4014d975f483781dc3430db2e6ffca5cb2
Landen Stephens <19829@oologah.k12.ok.us>
2018-08-26 18:41:56 -0500

    tests

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,12 @@
+const ytdl = require('ytdl-core')
+
 exports.run = (client, message, params) => {
 if(!params) {
     message.channel.send(`Please specify a song!`)
+} else {
+    var url = message.content.split(" ")
+    var time = '1:20'
+    client.musicQueue.set("params", [url, time])
 }
 
 };
